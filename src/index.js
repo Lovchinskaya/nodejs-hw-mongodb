19eb63ba4b1f53c5ba39e52634a69bd4e1b96eff
Katia <katia.lovchinskaya@gmail.com>
2025-05-12 09:21:06 +0300

    redone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-initMongoConnection();
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
+
+
+// initMongoConnection();
+// setupServer();
