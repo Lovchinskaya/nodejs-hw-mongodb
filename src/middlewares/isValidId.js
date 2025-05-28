30b73f404103473e04acb972c4464fe606d3a635
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2025-05-28 13:57:23 +0300

    first fix after done

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,7 @@ export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
 
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Invalid contact ID format');
+    throw createHttpError(500, 'Invalid contact ID format');
   }
 
   next();
